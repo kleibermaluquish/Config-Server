1356b9413395826068f7436329136390ebbef0b1
kleibermaluquish <maluquish.19@gmail.com>
2019-11-26 18:41:25 -0500

    Se realizaron cambio

--- src/main/java/com/empresa/app/serverconfig/ServerConfigApplication.java
+++ src/main/java/com/empresa/app/serverconfig/ServerConfigApplication.java
@@ -1,7 +1,5 @@
 package com.empresa.app.serverconfig;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
